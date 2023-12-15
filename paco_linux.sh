a76b45d1a959a426aa052995b93a627763e5b29c
ayoub elmouafik <145219022+mfk-ayoub@users.noreply.github.com>
2023-12-15 21:11:44 +0100

    Update paco_linux.sh

--- paco_linux.sh
+++ paco_linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "#Installing francinette by King SegFault The First"
+echo "#Installing francinette by iatmi"
 echo "#This is gonna take too long long int, Please !don't wait untill successfully installed"
 
 cd /nfs/sgoinfre/goinfre/Perso/$(whoami)/ && mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew && echo "export PATH=\"/nfs/sgoinfre/goinfre/Perso/$(whoami)/homebrew/bin:\$PATH\"" >> ~/.zshrc && source ~/.zshrc && brew install python && pip install GitPython && bash -c "$(curl -fsSL https://raw.github.com/xicodomingues/francinette/master/bin/install.sh)"
